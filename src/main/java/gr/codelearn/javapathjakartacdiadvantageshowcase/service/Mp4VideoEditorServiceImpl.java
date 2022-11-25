32a0f59564048c28dd34ff1f096ec1b7ba8b6981
IoannisKlian <giannisklian@gmail.com>
2022-11-25 15:44:41 +0200

    Add lifecycle callbacks

--- src/main/java/gr/codelearn/javapathjakartacdiadvantageshowcase/service/Mp4VideoEditorServiceImpl.java
+++ src/main/java/gr/codelearn/javapathjakartacdiadvantageshowcase/service/Mp4VideoEditorServiceImpl.java
@@ -1,13 +1,27 @@
 package gr.codelearn.javapathjakartacdiadvantageshowcase.service;
 
 import gr.codelearn.javapathjakartacdiadvantageshowcase.domain.Video;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import jakarta.enterprise.context.ApplicationScoped;
+import lombok.extern.slf4j.Slf4j;
 
 import java.math.BigDecimal;
 
 @ApplicationScoped
+@Slf4j
 public class Mp4VideoEditorServiceImpl implements VideoEditorService {
     private Integer randomViews = (int) (Math.random() * 10_000) + 100;
+
+    @PostConstruct
+    private void initialize() {
+        log.info("Class {} has been initialized.", getClass().getName());
+    }
+
+    @PreDestroy
+    private void destroy() {
+        log.info("Class {} is being destroyed.", getClass().getName());
+    }
 
     @Override
     public String edit(Video videoFile) {
